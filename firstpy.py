c953ab227fb4e43c9cf2ecc2abae2380548e09ab
ElhananGitHub <54417323+ElhananGitHub@users.noreply.github.com>
2019-08-27 14:01:46 -0500

    Archivo con ejemplos de Python 2

--- firstpy.py
+++ firstpy.py
@@ -10,3 +10,13 @@ def prueba(num):
 	return num*gad
 
 print(prueba(12))
+
+# Function to know wich students have enough credits and GPA to graduate
+
+def graduation_reqs(credits, gpa):
+  if credits >= 120 and gpa >= 2:
+    return "You meet the requirements to graduate!"
+  else:
+    return "Sorry, you have failed"
+  
+print(graduation_reqs(120,3))
